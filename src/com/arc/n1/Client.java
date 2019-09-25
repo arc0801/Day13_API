cd49112ae4deffdc87dfceeb9a9604d5603c9d48
arc0801 <54796547+arc0801@users.noreply.github.com>
2019-09-25 17:48:10 +0900

    2

---
+++ src/com/arc/n1/Client.java
@@ -0,0 +1,77 @@
+package com.arc.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		//1단계
+		Socket sc = null;
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+		Scanner ssc = new Scanner(System.in);
+
+
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+
+		try {
+			boolean check = true;
+			while(check) {
+				sc = new Socket("211.238.142.34", 8282);
+				System.out.println("서버로 전송할 메세지를 입력하세요");
+				String str = ssc.next();
+				os = sc.getOutputStream(); //byte
+				ow = new OutputStreamWriter(os);; //char
+				bw = new BufferedWriter(ow);
+				bw.write(str + "\r\n");
+				bw.flush();
+				System.out.println("서버로 전송 완료");
+
+				if(str.equals("q")) {
+					break;
+				}
+				System.out.println("클라이언트 메세지 받을 준비");
+				is = sc.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+				str = br.readLine();
+
+				if(str.equals("q")) {
+					break;
+				}
+				System.out.println(str);
+			}//while
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				bw.close();
+				ow.close();
+				os.close();
+				sc.close();
+				br.close();
+				ir.close();
+				is.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/com/arc/n1/Server.java
@@ -0,0 +1,83 @@
+package com.arc.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+	public static void main(String[] args) {
+		//ServerSocket
+		//Socket
+		ServerSocket ss = null;
+		Socket sc = null;
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+
+		Scanner ssc = new Scanner(System.in);
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+
+		try {
+			boolean check = true;
+			ss = new ServerSocket(8282);
+			while(check) {
+				System.out.println("클라이언트 접속 받을 준비 중");
+				sc =ss.accept();
+				is = sc.getInputStream(); //byte
+				ir = new InputStreamReader(is); //char
+				br = new BufferedReader(ir); //String
+				String str = br.readLine();
+
+				if(str.equals("q")) {
+					break;
+				}
+				System.out.println(str);
+
+				os = sc.getOutputStream();
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				System.out.println("클라이언트에 보낼 메세지 입력");
+				str = ssc.next();
+				bw.write(str + "\r\n");
+				bw.flush();
+				System.out.println("클라이언트로 전송 완료");
+
+				if(str.equals("q")) {
+					break;
+				}
+			}//while
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				ir.close();
+				is.close();
+				bw.close();
+				ow.close();
+				os.close();
+				sc.close();
+				ss.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+
+
+	}
+
+}
